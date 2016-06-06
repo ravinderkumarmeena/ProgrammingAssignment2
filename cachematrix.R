b97a9d33bdfa60ef898d73eefd7f72f79f38b04d
ravinderkumarmeena <raviayaan005@gmail.com>
2016-06-06 05:49:17 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+  ram<-NULL       ##for holding null value
+  set<-function(y) 
+  {
+    x<<-y
+    ram<<-NULL     #when matrix is assigned new value,we should have null as value
+  }
+  get<-function() {
+    x      #returns what is stored
+  }
+  inverseofset<-function(inverse)  {
+    ram<<-inverse 
+  }
+  inverseofget<-function() 
+  {
+    ram
+  }
+  list(set=get,get=get,inverseofset=inverseofset,inverseofget=inverseofget)  #return the list
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  ## Return a matrix that is the inverse of 'x'
+  ram <- x$getInverse()
+  if (!is.null(ram)) {
+    message("getting data")
+    return(ram)
+  }
+  matrix<- x$get()
+  inv <- solve(matrix, ...)
+  x$setInverse(ram)
+  ram
+ }
